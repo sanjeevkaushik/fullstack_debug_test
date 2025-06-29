5bd66f3142959ff9947ee68e884dce9962e2fcb4
sanjeevkaushik <sanjeev.kaushik@gmail.com>
2025-06-29 14:16:30 -0700

    - Added app to run at port 5000 - Added CORS policy to allow call from frontend - Fixed rating null check condition - Fixed sql query to make prepared statement to avoid sql injection risk

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,30 @@
+from contextlib import nullcontext
 
 # app.py - Mock Flask backend (with bugs)
 from flask import Flask, request, jsonify
 import sqlite3
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 @app.route("/feedback", methods=["GET"])
 def get_feedback():
     conn = sqlite3.connect('feedback.db')
     cursor = conn.cursor()
+
     rating = request.args.get("rating")
     sort = request.args.get("sort", "desc")
-    query = "SELECT * FROM feedback"
-    if rating:
-        query += f" WHERE rating = {rating}"  # <-- SQL injection risk
+    query = "SELECT message, rating FROM feedback"
+
+    print(rating)
+    if rating != "null":
+        query += f" WHERE rating = ? "  # <-- Prepared statement
     query += f" ORDER BY created_at {sort}"
-    feedback = cursor.execute(query).fetchall()
+    print(query)
+    if rating != "null":
+        feedback = cursor.execute(query, rating).fetchall()
+    else:
+        feedback = cursor.execute(query).fetchall()
     conn.close()
     return jsonify(feedback)
 
@@ -28,3 +37,6 @@ def post_feedback():
     conn.commit()
     conn.close()
     return jsonify({"status": "ok"})
+
+if __name__ == '__main__':
+    app.run(port=5000)
